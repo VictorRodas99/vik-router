15b54ba757ec95a139db801d37d68ceb736b1df3
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-25 18:00:53 -0300

    add basic functionality of a SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import './App.css'
+import { HomePage, AboutPage } from './components/Page'
+import { useState, useEffect } from 'react'
+
+const NAVIGATION_EVENT_TYPE = 'navigation'
+
+export function navigate ({ href }) {
+  window.history.pushState({}, '', href) // Cargamos la nueva url en el history (Sin recargar la página)
+  const navigationEvent = new Event(NAVIGATION_EVENT_TYPE)
+  window.dispatchEvent(navigationEvent) // Avisamos que se lanzó un evento de navegación (Custom event)
+}
 
 function App () {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname) // Agrega al estado la nueva url que se cambió por el navigate
+    }
+
+    window.addEventListener(NAVIGATION_EVENT_TYPE, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT_TYPE, onLocationChange)
+    }
+  }, [])
+
   return (
-    <h1>My own router!</h1>
+    <main>
+      <h1>My own router!</h1>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
