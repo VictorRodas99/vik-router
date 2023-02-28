6f5fdf8a6b836a24b73932685f30c1b823627b00
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-28 15:52:16 -0300

    implement Route component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,9 @@ import './App.css'
 import { HomePage, AboutPage } from './components/Page'
 import NotFound from './components/NotFound'
 import { Router } from './Router'
+import { Route } from './Route'
 
 const ROUTES = [
-  {
-    path: '/',
-    Component: HomePage
-  },
-  {
-    path: '/about',
-    Component: AboutPage
-  },
   {
     path: '/search/:query',
     Component: ({ params }) => <h4>Se buscó "{params.query}"</h4>
@@ -22,7 +15,10 @@ function App () {
   return (
     <main>
       <h1>My own router!</h1>
-      <Router routes={ROUTES} defaultComponent={NotFound} />
+      <Router routes={ROUTES} defaultComponent={NotFound}>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={AboutPage} />
+      </Router>
     </main>
   )
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Children } from 'react'
 import { EVENTS } from './config'
 import { match } from 'path-to-regexp' // Se agrega una librería para abstraernos del uso de regex para el procesamiento de rutas
 
-export function Router ({ routes = [], defaultComponent: DefaultComponent = () => null }) {
+export function Router ({ children, routes = [], defaultComponent: DefaultComponent = () => null }) {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
   let routeParams = {}
 
@@ -20,7 +20,14 @@ export function Router ({ routes = [], defaultComponent: DefaultComponent = () =
     }
   }, [])
 
-  const Component = routes.find(({ path }) => {
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const isRoute = type.name === 'Route'
+    return isRoute ? props : null
+  }) // Leer las props de los Route components que podrían estar dentro del Router component como children
+
+  const routesToUse = routes.concat(routesFromChildren)
+
+  const Component = routesToUse.find(({ path }) => {
     /* `match` devuelve una función que permite comparar rutas
         además de devolver en un contrato específico los params y otros elementos
     */
