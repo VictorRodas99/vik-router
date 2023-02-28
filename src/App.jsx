bdc20574951e3331cebb86865bc76d245fe1b496
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-28 13:40:12 -0300

    implement routes and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
 import './App.css'
 import { HomePage, AboutPage } from './components/Page'
-import { useState, useEffect } from 'react'
+import NotFound from './components/NotFound'
+import { Router } from './Router'
 
 export const NAVIGATION_EVENT_TYPE = 'navigation'
 
+const ROUTES = [
+  {
+    path: '/',
+    Component: HomePage
+  },
+  {
+    path: '/about',
+    Component: AboutPage
+  }
+]
+
 function App () {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname) // Agrega al estado la nueva url que se cambió por el navigate
-    }
-
-    window.addEventListener(NAVIGATION_EVENT_TYPE, onLocationChange)
-    window.addEventListener('popstate', onLocationChange) // Escucha el evento de navegación hacia atrás
-
-    return () => {
-      window.removeEventListener(NAVIGATION_EVENT_TYPE, onLocationChange)
-      window.removeEventListener('popstate', onLocationChange)
-    }
-  }, [])
-
   return (
     <main>
       <h1>My own router!</h1>
-      {currentPath === '/' && <HomePage />}
-      {currentPath === '/about' && <AboutPage />}
+      <Router routes={ROUTES} defaultComponent={NotFound} />
     </main>
   )
 }
