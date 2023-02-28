71b0a307ec05ad3428981b0c3e96c41b3035e83c
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-28 12:24:04 -0300

    extract code in different files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import './App.css'
 import { HomePage, AboutPage } from './components/Page'
 import { useState, useEffect } from 'react'
 
-const NAVIGATION_EVENT_TYPE = 'navigation'
-
-export function navigate ({ href }) {
-  window.history.pushState({}, '', href) // Cargamos la nueva url en el history (Sin recargar la página)
-  const navigationEvent = new Event(NAVIGATION_EVENT_TYPE)
-  window.dispatchEvent(navigationEvent) // Avisamos que se lanzó un evento de navegación (Custom event)
-}
+export const NAVIGATION_EVENT_TYPE = 'navigation'
 
 function App () {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
